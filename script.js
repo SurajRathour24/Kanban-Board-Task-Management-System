527c30f4616f9584f194aec46f3eeb7fb4fe8a04
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2026-01-29 21:14:34 +0530

    Drag Drop style

--- script.js
+++ script.js
@@ -2,39 +2,23 @@ const toDo = document.querySelector("#todo");
 const progress = document.querySelector("#in-progress");
 const done = document.querySelector("#done");
 
-const tasks = document.querySelectorAll(".task-list");
+let tasks = document.querySelectorAll('.task-list');
 
-tasks.forEach(task => {
-    task.setAttribute("draggable", "true");
+tasks.forEach(function(task){
+    task.addEventListener("drag", function(e){
+        // console.log("Dragging", e);
+    })
+})
 
-    task.addEventListener("dragstart", e => {
-        e.dataTransfer.setData("text/plain", e.target.id);
-    });
-});
 
-function addDragEvOnColumn(col) {
-    if (!col) return;
-
-    col.addEventListener("dragover", e => {
-        e.preventDefault();
-    });
-
-    col.addEventListener("dragenter", e => {
-        e.preventDefault();
-        col.classList.add("hover");
-    });
-
-    col.addEventListener("dragleave", () => {
+function addDragEventOnCol(col){
+    col.addEventListener("dragenter", (e) =>{
+        col.classList.add("hover")
+    })
+    col.addEventListener("dragleave", (e) =>{
         col.classList.remove("hover");
-    });
-
-    col.addEventListener("drop", e => {
-        e.preventDefault();
-        const id = e.dataTransfer.getData("text/plain");
-        const task = document.getElementById(id);
-        col.appendChild(task);
-        col.classList.remove("hover");
-    });
+    })
 }
-
-[toDo, progress, done].forEach(addDragEvOnColumn);
+addDragEventOnCol(toDo);
+addDragEventOnCol(progress);
+addDragEventOnCol(done);

43fc4b401703d0125354855ee67b4912e8127f7f
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2026-01-29 21:58:17 +0530

    Drag Drop Feature added

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const toDo = document.querySelector("#todo");
 const progress = document.querySelector("#in-progress");
 const done = document.querySelector("#done");
 
+let draggerItem = null;
+
 let tasks = document.querySelectorAll('.task-list');
 
 tasks.forEach(function(task){
     task.addEventListener("drag", function(e){
         // console.log("Dragging", e);
+        draggerItem = task
     })
 })
 
@@ -16,6 +19,16 @@ function addDragEventOnCol(col){
         col.classList.add("hover")
     })
     col.addEventListener("dragleave", (e) =>{
+        col.classList.remove("hover");
+    })
+
+    col.addEventListener("dragover", (e) =>{
+        e.preventDefault();
+    })
+
+    col.addEventListener("drop", (e) =>{
+        e.preventDefault();
+        col.appendChild(draggerItem);
         col.classList.remove("hover");
     })
 }
